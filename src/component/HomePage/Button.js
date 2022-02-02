6dfaa8c6295e2c0174b3c883f9d18b2060550ddc
arm-and <armandriy@gmail.com>
2022-02-02 13:19:15 +0200

    home page done j

---
+++ src/component/HomePage/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+import '../../style/home-stylesheet/Cards.css'
+export default function Button(){
+    return(
+        <div>
+            <button className='btn' type='button'>View more</button>
+        </div>
+    )
+};
---
+++ src/component/HomePage/data/BooksData.js
@@ -0,0 +1,20 @@
+export const books = [
+    {
+        id: 1,
+        image: 'https://www.ranok.com.ua/storage/cache/data/products/30187/ch1068001u-400-552.jpg',
+        title: 'Час магії',
+        author: 'Фьоллер Є.'
+    },
+    {
+        id: 2,
+        image: 'https://www.ranok.com.ua/storage/cache/data/465991-400-552.jpg',
+        title: 'Привиди Дому на пагорбі',
+        author: 'Ширлі Джексон'
+    },
+    {
+        id: 3,
+        image: 'https://www.ranok.com.ua/storage/cache/data/products/23660/fb664007u_vynokurov_angelpryglyada_obl_-400-552.jpg',
+        title: 'Ангел пригляду',
+        author: 'Винокуров О.'
+    }
+]
