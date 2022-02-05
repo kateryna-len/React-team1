f48feac576aa8f85256a741552a336556b7d16eb
kateryna-len <lenkaterynna057@gmail.com>
2022-02-05 21:03:29 +0200

    change and add some styles for catalog_page

---
+++ src/component/Catalog/CatalogBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CatalogBtn(){
+  return(
+    <div>
+    <button className='catalog-btn' type='button'>Купити</button>
+    </div>
+  )
+}
+
+export default  CatalogBtn;
--- src/component/Catalog/CatalogCard.js
+++ src/component/Catalog/CatalogCard.js
@@ -1,18 +1,26 @@
 import React from "react";
 import '../../style/catalog/CatalogCard.css';
-import Button from '../HomePage/Button';
+import { BiHeart } from "react-icons/bi";
+import CatalogBtn from "./CatalogBtn";
+
+
+
+
 
 export default function CatalogCard({item}) {
     return (
         <div className='single-card-catalog'>
             <img src={item.image} width='200px' height='250px'/>
+            <div className='catalog-box'>
+                <p className='cod'>Код: {item.cod}</p><p className='icon'><BiHeart/></p>
+            </div>
             <h3>{item.title}</h3>
-            <p>{item.author}</p>
-            <div className='price'>
-                <p><strong>Price:</strong></p><p>{item.price}{'грн'}</p>
+            <p className='author'>{item.author}</p>
+            <div className='catalog-box'>
+                <p><strong>Ціна:</strong></p><p>{item.price}{'грн'}</p>
             </div>
             <div className='btn-catalog'>
-                <Button/>
+                <CatalogBtn/>
             </div>
         </div>
     )
