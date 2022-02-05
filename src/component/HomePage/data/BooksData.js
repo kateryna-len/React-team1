58d2a502a5e9616db47e75fa02f04ebf1f3b80ea
kateryna-len <lenkaterynna057@gmail.com>
2022-02-05 13:50:49 +0200

    update data file

--- src/component/HomePage/data/BooksData.js
+++ src/component/HomePage/data/BooksData.js
@@ -3,18 +3,29 @@ export const books = [
         id: 1,
         image: 'https://www.ranok.com.ua/storage/cache/data/products/30187/ch1068001u-400-552.jpg',
         title: 'Час магії',
-        author: 'Фьоллер Є.'
+        author: 'Фьоллер Є.',
+        price: 182
+        
     },
     {
         id: 2,
         image: 'https://www.ranok.com.ua/storage/cache/data/465991-400-552.jpg',
         title: 'Привиди Дому на пагорбі',
-        author: 'Ширлі Джексон'
+        author: 'Ширлі Джексон',
+        price: 320
     },
     {
         id: 3,
         image: 'https://www.ranok.com.ua/storage/cache/data/products/23660/fb664007u_vynokurov_angelpryglyada_obl_-400-552.jpg',
         title: 'Ангел пригляду',
-        author: 'Винокуров О.'
+        author: 'Винокуров О.',
+        price: 145
+    },
+    {
+        id: 4,
+        image: 'https://www.ranok.com.ua/storage/cache/data/products/25204/fb722076u_chystayaarhytektura_obl_3d-400-552.jpg',
+        title: 'Чиста архітектура',
+        author: 'Мартін Р.',
+        price: 440
     }
 ]
