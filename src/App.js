9e42608fadedb25086347fa6aa645f3b537b7d27
arm-and <armandriy@gmail.com>
2022-02-09 00:49:23 +0200

    added search by (useContext,useMemo)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React from 'react'
-import{Routes, Route} from 'react-router-dom'
+import React, {useState, useEffect,useMemo, useContext} from 'react'
+import {Routes, Route} from 'react-router-dom'
 import Cart from './component/Cart/Cart';
 import Catalog from './component/Catalog/Catalog';
 import Footer from './component/Footer/Footer';
 import Header from './component/Header/Header'
 import Home from './component/HomePage/Home';
 import './App.css'
+import BooksContext from './BooksContext';
 
 function App() {
-  return (
-    <div className='main-box'>
-      <div>
-        <Header/>
-      </div>
-      <div className='content'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/catalog'  element={<Catalog/>} />
-        <Route path='/cart' element={<Cart/>}/>
-      </Routes>
-          <div className='footer-content'>
-              <Footer/>
-          </div>
-      </div>
-    </div>
-  )
+    const [searchedBook, setSearchedBook] = useState('');
+    const providerValue = useMemo(
+        () => ({ searchedBook, setSearchedBook }),
+        [searchedBook]
+    );
+
+    return (
+        <BooksContext.Provider value={providerValue}>
+            <div className='main-box'>
+                <div>
+                    <Header/>
+                </div>
+                <div className='content'>
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/catalog' element={<Catalog/>}/>
+                        <Route path='/cart' element={<Cart/>}/>
+                    </Routes>
+                    <div className='footer-content'>
+                        <Footer/>
+                    </div>
+                </div>
+            </div>
+        </BooksContext.Provider>
+    )
 }
 
 export default App;
--- src/component/Catalog/CatalogCards.js
+++ src/component/Catalog/CatalogCards.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useMemo, useState, useContext} from 'react';
 import CatalogCard from './CatalogCard';
 import {books} from '../HomePage/data/BooksData';
 import '../../style/catalog/CatalogCards.css';
+import BooksContext from '../../BooksContext';
 
 export default function CatalogCards() {
+    const {searchedBook} = useContext(BooksContext);
+
+    const filteredBooks = books.filter(book => {
+        return book.title.toLowerCase().includes(searchedBook)
+            || book.author.toLowerCase().includes(searchedBook)
+    })
+    const filteredData = searchedBook ? filteredBooks : books;
     return (
         <div className='catalog-cards-container'>
             <div className='cards'>
                 {
-                    books.map(value => <CatalogCard key={value.id} item={value}/>)
+                    filteredData.map(value => <CatalogCard key={value.id} item={value}/>)
                 }
             </div>
         </div>
